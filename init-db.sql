dd34cd67d64d40a03d61530e488401b78dff4732
unknown <angela6309gonzalez@gmail.com>
2026-06-07 14:56:22 -0500

    feat: completar entorno de pruebas con dashboard-service y file-service (US-17)

--- init-db.sql
+++ init-db.sql
@@ -4,3 +4,4 @@ SELECT 'CREATE DATABASE circleguard_identity' WHERE NOT EXISTS (SELECT FROM pg_d
 SELECT 'CREATE DATABASE circleguard_promotion' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'circleguard_promotion')\gexec
 SELECT 'CREATE DATABASE circleguard_dashboard' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'circleguard_dashboard')\gexec
 SELECT 'CREATE DATABASE circleguard_form' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'circleguard_form')\gexec
+SELECT 'CREATE DATABASE circleguard_file' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'circleguard_file')\gexec
